f31cbcfd5a971d042705df041aef714559aa033c
Ivan Dryanovski <ivan.dryanovski@gmail.com>
2013-04-16 10:35:00 -0400

    adding graph tools

---
+++ src/graph/keyframe_graph_solver.cpp
@@ -0,0 +1,38 @@
+/**
+ *  @file keyframe_graph_solver.cpp
+ *  @author Ivan Dryanovski <ivan.dryanovski@gmail.com>
+ * 
+ *  @section LICENSE
+ * 
+ *  Copyright (C) 2013, City University of New York
+ *  CCNY Robotics Lab <http://robotics.ccny.cuny.edu>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "rgbdtools/graph/keyframe_graph_solver.h"
+
+namespace rgbdtools {
+
+KeyframeGraphSolver::KeyframeGraphSolver()
+{
+
+}
+
+KeyframeGraphSolver::~KeyframeGraphSolver()
+{
+
+}
+
+} // namespace rgbdtools
